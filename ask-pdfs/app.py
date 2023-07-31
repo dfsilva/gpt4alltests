e60b249d76b632e0c54ce662dcf3d66da4c230ea
Diego Silva <diegosiuniube@gmail.com>
2023-07-30 21:02:34 -0300

    tets

--- ask-pdfs/app.py
+++ ask-pdfs/app.py
@@ -2,20 +2,28 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
+from langchain.llms import HuggingFaceHub, GPT4All
+from langchain.callbacks import StdOutCallbackHandler
 
-def get_pdf_text(pdf_docs):
+def get_text_from_docs(docs):
     text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+    for doc in docs:
+        file_extension = doc.name.split('.')[-1]
+        if file_extension == "pdf":
+            pdf_reader = PdfReader(doc)
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+        elif file_extension == "csv":
+            st.write(f"You uploaded a CSV file: {doc.name}")
+        elif file_extension == "txt":
+            text += doc.read().decode()
+        else:
+            st.write(f"Unsupported file format: {doc.name}. Please upload a PDF, CSV, or TXT file.")
     return text
 
 
@@ -31,18 +39,22 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    # embeddings = OpenAIEmbeddings()
     embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":2048})
+    # llm = GPT4All(model="/Users/diegosilva/Library/Application Support/nomic.ai/GPT4All/ggml-gpt4all-j-v1.3-groovy.bin",
+    #             max_tokens=2000,
+    #             backend="gptj",
+    #             verbose=True,
+    #             callbacks=[StdOutCallbackHandler()],
+    #             n_threads=8,  # Change this according to your cpu threads
+    #             temp=0.5,)  
 
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(),
@@ -51,7 +63,7 @@ def get_conversation_chain(vectorstore):
     return conversation_chain
 
 
-def handle_userinput(user_question):
+def handle_user_input(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
 
@@ -66,8 +78,7 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
+    st.set_page_config(page_title="Chat with multiple Documents", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
@@ -75,30 +86,29 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
+    st.header("Chat with multiple documents :books:")
     user_question = st.text_input("Ask a question about your documents:")
     if user_question:
-        handle_userinput(user_question)
+        handle_user_input(user_question)
 
     with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        st.subheader("Your documents PDF/CSV/TXT")
+        docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", type=["pdf", "csv", "txt"], accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
-
+                # get get texts
+                raw_text = get_text_from_docs(docs)
+                st.write(raw_text)
+            with st.spinner("Getting text chunks..."):
                 # get the text chunks
                 text_chunks = get_text_chunks(raw_text)
-
+            with st.spinner("Getting vector store..."):
                 # create vector store
                 vectorstore = get_vectorstore(text_chunks)
-
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
-
+            with st.spinner("Getting conversation chain..."):
+                # create conversation chain  
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
 if __name__ == '__main__':
     main()
